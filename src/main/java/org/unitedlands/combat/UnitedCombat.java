9963b8fcc9e3306356a075f464d6864c6e1866f9
Litning11 <dario.mavilia@outlook.com>
2025-03-23 23:30:26 +0000

    Added anchor nerf.

--- src/main/java/org/unitedlands/combat/UnitedCombat.java
+++ src/main/java/org/unitedlands/combat/UnitedCombat.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.unitedlands.combat.commands.PvPCmd;
 import org.unitedlands.combat.commands.ReloadCmd;
 import org.unitedlands.combat.hooks.Placeholders;
+import org.unitedlands.combat.listeners.ExplosionListener;
 import org.unitedlands.combat.listeners.PlayerListener;
 import org.unitedlands.combat.listeners.TownyListener;
 
@@ -30,8 +31,8 @@ public final class UnitedCombat extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         registerListeners();
-        // Register the reload command using ReloadCmd as its executor & tab completer.
-        registerCommand(new ReloadCmd(this), new ReloadCmd(this));
+        ReloadCmd reloadCmd = new ReloadCmd(this);
+        registerCommand(reloadCmd, reloadCmd);
     }
 
     private void registerListeners() {
@@ -39,13 +40,13 @@ public final class UnitedCombat extends JavaPlugin {
         PluginManager pluginManager = Bukkit.getServer().getPluginManager();
         pluginManager.registerEvents(new PlayerListener(this), this);
         pluginManager.registerEvents(new TownyListener(this), this);
+        pluginManager.registerEvents(new ExplosionListener(getConfig()), this);
         Objects.requireNonNull(getCommand("pvp")).setExecutor(new PvPCmd());
 
         // PlaceholderAPI Expansion Register
         if (pluginManager.getPlugin("PlaceholderAPI") != null) {
             new Placeholders().register();
         }
-
     }
 
     public void reloadPluginConfig() {
@@ -55,6 +56,7 @@ public final class UnitedCombat extends JavaPlugin {
         PluginManager pluginManager = Bukkit.getServer().getPluginManager();
         pluginManager.registerEvents(new PlayerListener(this), this);
         pluginManager.registerEvents(new TownyListener(this), this);
+        pluginManager.registerEvents(new ExplosionListener(getConfig()), this);
         getLogger().info("Plugin configuration reloaded.");
     }
 
--- src/main/java/org/unitedlands/combat/listeners/ExplosionListener.java
+++ src/main/java/org/unitedlands/combat/listeners/ExplosionListener.java
@@ -1,11 +1,18 @@
 package org.unitedlands.combat.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.RespawnAnchor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ExplosionListener implements Listener {
     private final FileConfiguration config;
@@ -30,4 +37,23 @@ public class ExplosionListener implements Listener {
         }
     }
 
+    // Cancel charging of respawn anchors if already at or above 4 charges, so they can't explode.
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Ensure the interact event to just charging anchors.
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        Block clickedBlock = event.getClickedBlock();
+        if (clickedBlock == null || clickedBlock.getType() != Material.RESPAWN_ANCHOR) return;
+        if (event.getItem() == null || event.getItem().getType() != Material.GLOWSTONE) return;
+        if (clickedBlock.getWorld().getEnvironment() == World.Environment.NETHER) return;
+
+        // Retrieve the respawn anchor block data.
+        BlockData data = clickedBlock.getBlockData();
+        if (!(data instanceof RespawnAnchor anchor)) return;
+
+        // If the anchor is already charged with 4 or more charges, cancel.
+        if (anchor.getCharges() >= 4) {
+            event.setCancelled(true);
+        }
+    }
 }
