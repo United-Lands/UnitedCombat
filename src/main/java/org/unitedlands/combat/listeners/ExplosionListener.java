ab4ab5dc15139f1d01e69491064881f799203915
Litning11 <dario.mavilia@outlook.com>
2025-03-24 00:15:14 +0000

    Nerf TNT Minecarts.

--- src/main/java/org/unitedlands/combat/listeners/ExplosionListener.java
+++ src/main/java/org/unitedlands/combat/listeners/ExplosionListener.java
@@ -8,6 +8,7 @@ import org.bukkit.block.data.type.RespawnAnchor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.ExplosiveMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -23,21 +24,21 @@ public class ExplosionListener implements Listener {
 
     // End Crystals
     @EventHandler
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        // Check if a player is damaged by an End Crystal
+    public void onEntityDamageByCrystal(EntityDamageByEntityEvent event) {
+        // Check if a player is damaged by an End Crystal.
         if (event.getEntity() instanceof Player && event.getDamager() instanceof EnderCrystal) {
-            // Check if nerf-crystal-damage is enabled
+            // Check if nerf-crystal-damage is enabled.
             if (config.getBoolean("end-crystals.nerf-crystal-damage", true)) {
                 double damageMultiplier = config.getDouble("end-crystals.crystal-damage-percent", 100) / 100.0;
-                // Apply custom damage multiplier
+                // Apply custom damage multiplier.
                 event.setDamage(event.getDamage() * damageMultiplier);
             }
         } else {
-            // If nerf-crystal-damage is false, ensure vanilla behavior, intentionally left blank
+            // If nerf-crystal-damage is false, ensure vanilla behavior, intentionally left blank.
         }
     }
 
-    // Cancel charging of respawn anchors if already at or above 4 charges, so they can't explode.
+    // Respawn Anchors.
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         // Ensure the interact event to just charging anchors.
@@ -53,5 +54,21 @@ public class ExplosionListener implements Listener {
         if (!(data instanceof RespawnAnchor)) return;
 
         event.setCancelled(true);
+    }
+
+    // TNT Minecarts
+    @EventHandler
+    public void onEntityDamageByMinecart(EntityDamageByEntityEvent event) {
+        // Ensure the damaged entity is a player.
+        if (!(event.getEntity() instanceof Player)) return;
+        // Ensure the damager is a TNT minecart.
+        if (!(event.getDamager() instanceof ExplosiveMinecart)) return;
+        // Check if nerf-minecart-damage is enabled.
+        if (config.getBoolean("minecarts.nerf-minecart-damage", true)) {
+            double damageMultiplier = config.getDouble("minecarts.minecart-damage-percent", 100) / 100.0;
+            event.setDamage(event.getDamage() * damageMultiplier);
+        } else {
+            // If nerf-minecart-damage is false, ensure vanilla behavior, intentionally left blank.
+        }
     }
 }
